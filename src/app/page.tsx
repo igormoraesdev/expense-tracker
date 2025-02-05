c3f33cde278b59bb6183efbc1ffb487c90434f93
Igor Moraes <igormoraesdev@gmail.com>
2025-02-04 21:03:59 -0300

    chore: update form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,8 @@ import { SigninForm } from "@/components/SiginForm";
 export default function Home() {
   return (
     <section className="bg-white w-full h-svh flex items-center justify-center">
-      <div className="w-full mx-auto max-w-screen-lg px-4 py-4">
-        <div className="mx-auto max-w-lg">
-          <SigninForm />
-        </div>
+      <div className="w-full max-w-4xl">
+        <SigninForm />
       </div>
     </section>
   );
--- src/components/SiginForm.tsx
+++ src/components/SiginForm.tsx
@@ -1,108 +1,167 @@
 "use client";
 
-import { Button } from "./ui/button";
-import { Icons } from "./ui/icon";
-
 export function SigninForm() {
   return (
-    <form
-      action="#"
-      className="mt-6 mb-0 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
-    >
-      <p className="text-center text-lg font-medium">Sign in to your account</p>
+    <div className="w-full font-[sans-serif]">
+      <div className="w-full min-h-screen flex flex-col items-center justify-center">
+        <div className="grid md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl max-md:max-w-lg w-full px-4 py-8 lg:m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
+          <div className="w-full px-4 py-4">
+            <form>
+              <div className="mb-12">
+                <h3 className="text-gray-800 text-3xl font-extrabold">
+                  Sign in
+                </h3>
+                <p className="text-sm mt-4 text-gray-800">
+                  Don't have an account{" "}
+                  <a
+                    href="javascript:void(0);"
+                    className="text-blue-600 font-semibold hover:underline ml-1 whitespace-nowrap"
+                  >
+                    Register here
+                  </a>
+                </p>
+              </div>
 
-      <div>
-        <label htmlFor="email" className="sr-only">
-          Email
-        </label>
+              <div>
+                <label className="text-gray-800 text-xs block mb-2">
+                  Email
+                </label>
+                <div className="relative flex items-center">
+                  <input
+                    name="email"
+                    type="text"
+                    required
+                    className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 pl-2 pr-8 py-3 outline-none"
+                    placeholder="Enter email"
+                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#bbb"
+                    stroke="#bbb"
+                    className="w-[18px] h-[18px] absolute right-2"
+                    viewBox="0 0 682.667 682.667"
+                  >
+                    <defs>
+                      <clipPath id="a" clipPathUnits="userSpaceOnUse">
+                        <path
+                          d="M0 512h512V0H0Z"
+                          data-original="#000000"
+                        ></path>
+                      </clipPath>
+                    </defs>
+                    <g
+                      clipPath="url(#a)"
+                      transform="matrix(1.33 0 0 -1.33 0 682.667)"
+                    >
+                      <path
+                        fill="none"
+                        strokeMiterlimit="10"
+                        strokeWidth="40"
+                        d="M452 444H60c-22.091 0-40-17.909-40-40v-39.446l212.127-157.782c14.17-10.54 33.576-10.54 47.746 0L492 364.554V404c0 22.091-17.909 40-40 40Z"
+                        data-original="#000000"
+                      ></path>
+                      <path
+                        d="M472 274.9V107.999c0-11.027-8.972-20-20-20H60c-11.028 0-20 8.973-20 20V274.9L0 304.652V107.999c0-33.084 26.916-60 60-60h392c33.084 0 60 26.916 60 60v196.653Z"
+                        data-original="#000000"
+                      ></path>
+                    </g>
+                  </svg>
+                </div>
+              </div>
 
-        <div className="relative">
-          <input
-            type="email"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
-            placeholder="Enter email"
-          />
+              <div className="mt-8">
+                <label className="text-gray-800 text-xs block mb-2">
+                  Password
+                </label>
+                <div className="relative flex items-center">
+                  <input
+                    name="password"
+                    type="password"
+                    required
+                    className="w-full text-gray-800 text-sm border-b border-gray-300 focus:border-blue-600 pl-2 pr-8 py-3 outline-none"
+                    placeholder="Enter password"
+                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="#bbb"
+                    stroke="#bbb"
+                    className="w-[18px] h-[18px] absolute right-2 cursor-pointer"
+                    viewBox="0 0 128 128"
+                  >
+                    <path
+                      d="M64 104C22.127 104 1.367 67.496.504 65.943a4 4 0 0 1 0-3.887C1.367 60.504 22.127 24 64 24s62.633 36.504 63.496 38.057a4 4 0 0 1 0 3.887C126.633 67.496 105.873 104 64 104zM8.707 63.994C13.465 71.205 32.146 96 64 96c31.955 0 50.553-24.775 55.293-31.994C114.535 56.795 95.854 32 64 32 32.045 32 13.447 56.775 8.707 63.994zM64 88c-13.234 0-24-10.766-24-24s10.766-24 24-24 24 10.766 24 24-10.766 24-24 24zm0-40c-8.822 0-16 7.178-16 16s7.178 16 16 16 16-7.178 16-16-7.178-16-16-16z"
+                      data-original="#000000"
+                    ></path>
+                  </svg>
+                </div>
+              </div>
+              <div className="mt-12">
+                <button
+                  type="button"
+                  className="w-full shadow-xl py-2.5 px-4 text-sm tracking-wide rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                >
+                  Sign in
+                </button>
+              </div>
 
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-          </span>
+              <div className="my-4 flex items-center gap-4">
+                <hr className="w-full border-gray-300" />
+                <p className="text-sm text-gray-800 text-center">or</p>
+                <hr className="w-full border-gray-300" />
+              </div>
+
+              <div className="space-x-6 flex justify-center">
+                <button type="button" className="border-none outline-none">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-7 h-7 inline"
+                    viewBox="0 0 512 512"
+                  >
+                    <path
+                      fill="#fbbd00"
+                      d="M120 256c0-25.367 6.989-49.13 19.131-69.477v-86.308H52.823C18.568 144.703 0 198.922 0 256s18.568 111.297 52.823 155.785h86.308v-86.308C126.989 305.13 120 281.367 120 256z"
+                      data-original="#fbbd00"
+                    />
+                    <path
+                      fill="#0f9d58"
+                      d="m256 392-60 60 60 60c57.079 0 111.297-18.568 155.785-52.823v-86.216h-86.216C305.044 385.147 281.181 392 256 392z"
+                      data-original="#0f9d58"
+                    />
+                    <path
+                      fill="#31aa52"
+                      d="m139.131 325.477-86.308 86.308a260.085 260.085 0 0 0 22.158 25.235C123.333 485.371 187.62 512 256 512V392c-49.624 0-93.117-26.72-116.869-66.523z"
+                      data-original="#31aa52"
+                    />
+                    <path
+                      fill="#3c79e6"
+                      d="M512 256a258.24 258.24 0 0 0-4.192-46.377l-2.251-12.299H256v120h121.452a135.385 135.385 0 0 1-51.884 55.638l86.216 86.216a260.085 260.085 0 0 0 25.235-22.158C485.371 388.667 512 324.38 512 256z"
+                      data-original="#3c79e6"
+                    />
+                    <path
+                      fill="#cf2d48"
+                      d="m352.167 159.833 10.606 10.606 84.853-84.852-10.606-10.606C388.668 26.629 324.381 0 256 0l-60 60 60 60c36.326 0 70.479 14.146 96.167 39.833z"
+                      data-original="#cf2d48"
+                    />
+                    <path
+                      fill="#eb4132"
+                      d="M256 120V0C187.62 0 123.333 26.629 74.98 74.98a259.849 259.849 0 0 0-22.158 25.235l86.308 86.308C162.883 146.72 206.376 120 256 120z"
+                      data-original="#eb4132"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <div className="w-full h-full flex items-center bg-[#000842] rounded-xl p-8">
+            <img
+              src="https://readymadeui.com/signin-image.webp"
+              className="w-full aspect-[12/12] object-contain"
+              alt="login-image"
+            />
+          </div>
         </div>
       </div>
-
-      <div>
-        <label htmlFor="password" className="sr-only">
-          Password
-        </label>
-
-        <div className="relative">
-          <input
-            type="password"
-            className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-xs"
-            placeholder="Enter password"
-          />
-
-          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-4 text-gray-400"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-              />
-            </svg>
-          </span>
-        </div>
-      </div>
-
-      <div className="flex flex-1 flex-col items-center gap-2">
-        <Button
-          disabled
-          type="submit"
-          className="w-full rounded-lg p-6 text-sm font-medium"
-        >
-          Sign in
-        </Button>
-        <Button
-          className="w-full rounded-lg bg-primary p-6 text-sm text-white font-medium"
-          onClick={() => console.log("click")}
-          variant="default"
-        >
-          <Icons.google className="mr-2 h-4 w-4" />
-          Signin with Google
-        </Button>
-      </div>
-
-      <p className="text-center text-sm text-gray-500">
-        No account?{" "}
-        <a className="underline" href="#">
-          Sign up
-        </a>
-      </p>
-    </form>
+    </div>
   );
 }
