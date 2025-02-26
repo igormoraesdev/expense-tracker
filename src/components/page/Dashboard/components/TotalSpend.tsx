6e6c83e7ce5ed96d82ec0e4096d9bb00ea13eeed
Igor Moraes <igormoraesdev@gmail.com>
2025-02-26 16:01:34 -0300

    chore: update card

--- src/components/page/Dashboard/components/TotalSpend.tsx
+++ src/components/page/Dashboard/components/TotalSpend.tsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { useDashboardData } from "@/hooks/useDashboardData";
-import { CircleDollarSign } from "lucide-react";
+import { DollarSign } from "lucide-react";
 import { useMemo } from "react";
 
 export const TotalSpend = () => {
@@ -18,16 +18,16 @@ export const TotalSpend = () => {
 
   return (
     <div className="flex flex-row justify-between items-center p-6 border-2 border-indigo-200 rounded-xl w-full max-w-[340px] h-[140px] shadow-xl">
-      <div>
-        <p className="mb-1 text-sm font-medium">Total spend this month</p>
+      <div className="flex flex-col flex-1 items-center">
+        <p className="mb-1 text-sm font-bold">Total spend this month</p>
         {isLoading ? (
           <Skeleton className="h-[36px] w-[195px] rounded-xl" />
         ) : (
-          <p className="text-3xl font-medium text-red-600">{currencyData}</p>
+          <p className="text-3xl font-bold text-red-600">{currencyData}</p>
         )}
       </div>
 
-      <CircleDollarSign size={24} className="text-indigo-700" />
+      <DollarSign size={30} className="text-indigo-600" />
     </div>
   );
 };
