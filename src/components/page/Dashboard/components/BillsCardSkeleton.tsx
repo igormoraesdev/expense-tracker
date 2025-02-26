36d01ae08e46e073ab917a3779a45e6fc77718cf
Igor Moraes <igormoraesdev@gmail.com>
2025-02-26 13:00:04 -0300

    chore: update style

--- src/components/page/Dashboard/components/BillsCardSkeleton.tsx
+++ src/components/page/Dashboard/components/BillsCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export const BillsCardSkeleton = () => {
   return (
-    <div className="grid w-full p-6 sm:p-8 border-2 border-gray-200 rounded-md gap-4">
+    <div className="grid w-full p-4 border-2 border-gray-200 rounded-md gap-4">
       <div className="flex items-start sm:items-center justify-between">
         <div className="flex flex-col sm:flex-row items-start sm:items-center gap-4">
           <Skeleton className="flex items-center justify-center rounded-full h-[36px] w-[36px] sm:h-[48px] sm:w-[48px]" />
