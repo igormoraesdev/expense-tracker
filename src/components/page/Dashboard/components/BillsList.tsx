6c39e0533c0c5f52772f13507156e34c43554fb3
Igor Moraes <igormoraesdev@gmail.com>
2025-02-26 15:39:11 -0300

    chore: update grid

--- src/components/page/Dashboard/components/BillsList.tsx
+++ src/components/page/Dashboard/components/BillsList.tsx
@@ -32,7 +32,7 @@ export const BillsList = ({ onOpenDialog, onSelectBill }: BillsListProps) => {
   return (
     <div className="flex flex-col">
       <h3 className="text-2xl font-bold">Bills</h3>
-      <div className="flex flex-col gap-8 py-4">
+      <div className="grid grid-cols-1 2xl:grid-cols-2 gap-8 py-4">
         {isLoading ? (
           <>
             {Array.from(new Array(1)).map((_, index) => (
