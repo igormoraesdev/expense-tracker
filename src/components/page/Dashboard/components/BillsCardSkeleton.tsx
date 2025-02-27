ef53fbc1215183d5e7c03fe7f66fc46d4e4c7924
Igor Moraes <igormoraesdev@gmail.com>
2025-02-27 16:02:40 -0300

    chore: update skeletons

--- src/components/page/Dashboard/components/BillsCardSkeleton.tsx
+++ src/components/page/Dashboard/components/BillsCardSkeleton.tsx
@@ -5,7 +5,7 @@ export const BillsCardSkeleton = () => {
     <div className="grid min-h-[99px] w-full p-4 border-2 border-indigo-100 rounded-xl shadow-xl gap-4">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4">
-          <Skeleton className="rounded-full h-[36px] w-[36px] sm:h-[48px] sm:w-[48px]" />
+          <Skeleton className="h-[16px] w-[16px]" />
           <Skeleton className="h-[20px] w-[60px]" />
           <Skeleton className="h-[22px] w-[60px]" />
           <Skeleton className="h-[22px] w-[60px]" />
--- src/components/page/Dashboard/components/HighExpenseChartSkeleton.tsx
+++ src/components/page/Dashboard/components/HighExpenseChartSkeleton.tsx
@@ -1,12 +1,5 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const HighExpenseChartSkeleton = () => {
-  return (
-    <div className="grid grid-cols-4 items-end justify-between w-full p-6 sm:p-8 gap-4 max-h-[400px]">
-      <Skeleton className="h-[100px] w-full" />
-      <Skeleton className="h-[250px] w-full" />
-      <Skeleton className="h-[350px] w-full" />
-      <Skeleton className="h-[150px] w-full" />
-    </div>
-  );
+  return <Skeleton className="h-[400px] w-full" />;
 };
