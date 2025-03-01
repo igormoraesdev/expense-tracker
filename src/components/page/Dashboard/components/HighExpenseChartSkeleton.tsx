6341492387294856c3709be34cf7b09c34a82a06
Igor Moraes <igormoraesdev@gmail.com>
2025-03-01 15:40:12 -0300

    chore: update skeleton

--- src/components/page/Dashboard/components/HighExpenseChartSkeleton.tsx
+++ src/components/page/Dashboard/components/HighExpenseChartSkeleton.tsx
@@ -1,5 +1,5 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export const HighExpenseChartSkeleton = () => {
-  return <Skeleton className="h-[400px] w-full" />;
+  return <Skeleton className="h-[600px] sm:h-[400px] w-full" />;
 };
